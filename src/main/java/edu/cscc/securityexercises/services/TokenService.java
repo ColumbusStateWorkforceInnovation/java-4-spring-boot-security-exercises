f17869b7f04cbc2bb9bb59418e7a659bd1f57a56
Jim Kirkbide <jkirkbride@cscc.edu>
2024-06-19 16:44:38 -0400

    Enabling method-level security. Correcting TokenService.

--- src/main/java/edu/cscc/securityexercises/services/TokenService.java
+++ src/main/java/edu/cscc/securityexercises/services/TokenService.java
@@ -1,5 +1,7 @@
 package edu.cscc.securityexercises.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
@@ -8,22 +10,23 @@ import java.util.List;
 @Service
 public class TokenService {
 
+    Logger logger = LoggerFactory.getLogger(TokenService.class);
+
     public boolean hasScope(Jwt jwt, String scope) {
         String[] scopes = jwt.getClaimAsString("scope").split(" ");
+        logger.info("Checking if scope {} is in scopes {}", scope, scopes);
         List<String> scopeList = List.of(scopes);
         return scopeList.contains(scope);
     }
 
-    public boolean hasScopes(Jwt jwt, String[] scopes) {
-        for (String scope : scopes) {
+    public boolean hasScopes(Jwt jwt, String scopes) {
+        String[] scopeArray = scopes.split(" ");
+        logger.info("Checking if scopes {} are in scopes {}", scopes, jwt.getClaimAsString("scope"));
+        for (String scope : scopeArray) {
             if (!hasScope(jwt, scope)) {
                 return false;
             }
         }
         return true;
-    }
-
-    public String getClaim(Jwt jwt, String claim) {
-        return jwt.getClaim(claim);
     }
 }
